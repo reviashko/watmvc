82e6e994167f44e15561f744108ee0cbdbfba9f3
reviashko <devers@inbox.ru>
2015-11-17 18:19:36 +0300

    added catalog controller

---
+++ WatMVC/WatMvc/Controllers/CatalogController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WatMvc.Views
+{
+    public class CatalogController : Controller
+    {
+        //
+        // GET: /Catalog/e-333-5f/
+        [Route("catalog/{article}")]
+        public ActionResult Index(string article)
+        {
+            ViewBag.Message = string.Format("Your article is : {0}", article);
+            return View();
+        }
+
+	}
+}
