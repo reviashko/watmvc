827a487ac5a2959bdc4b4559f534341d76d682dc
reviashko <devers@inbox.ru>
2015-11-17 16:24:44 +0300

    1

---
+++ WatMVC/WatMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WatMvc.MvcApplication" Language="C#" %>
---
+++ WatMVC/WatMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WatMvc.Startup))]
+namespace WatMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
